fc33e6bb165155e2dc5bff8ce8b263cabc8df9d6
heatheraislinn <heatheraislinn@gmail.com>
2018-12-08 11:43:26 -0500

    Add structure and content and begin styling

--- src/components/PortfolioPage.vue
+++ src/components/PortfolioPage.vue
@@ -31,56 +31,99 @@
         <section id="projects">
             <h2 class="projects-header">Check out what I've worked on</h2>
             <div class="project-tile">
-                <a href="https://codepen.io/FreeCodeCamp/pen/NNvBQW" target="_blank">
+
+                <div class="container">
                     <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687690-997e4480-fad4-11e8-9747-4a7d1a7c2383.png" alt="project">
-                </a>
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/mQgPMz" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
 
-                <a href="https://codepen.io/FreeCodeCamp/pen/ONjoLe" target="_blank">
+                <div class="container">
                     <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687697-b0249b80-fad4-11e8-93d7-41fa3123ba86.png" alt="project">
-                </a>
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/rQEMoP" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
 
-                <a href="https://codepen.io/FreeCodeCamp/pen/PNKdjo" target="_blank">
+                <div class="container">
+                    <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687812-8ec4af00-fad6-11e8-8793-046bd3a5ad47.png" alt="project">
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/rQXZpd" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <img class="project-pic" src="https://adlantern.app/assets/images/Site-Demo.gif" alt="project">
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://adlantern.app/" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
                     <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687687-91bea000-fad4-11e8-809b-595d15e6008f.gif" alt="project">
-                </a>
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/BGbgMR" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
 
-                <a href="https://codepen.io/FreeCodeCamp/pen/mVEJag" target="_blank">
+                <div class="container">
                     <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687682-7fdcfd00-fad4-11e8-8c96-6220386c8f19.gif" alt="project">
-                </a>
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/OareZO" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="container">
+                    <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687793-4ad1aa00-fad6-11e8-9b35-c82ec6f1b42d.gif" alt="project">
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/heatheraislinn/pen/aQRaRK" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
 
-                <a href="https://codepen.io/FreeCodeCamp/pen/wGqEga" target="_blank">
-                    <img class="project-pic" src="https://cloud.githubusercontent.com/assets/15967809/17642774/7d091806-6171-11e6-8d47-ecf2f2833fe2.png" alt="project">
-                </a>
-
-                <a href="https://codepen.io/FreeCodeCamp/pen/KzXQgy" target="_blank">
-                    <img class="project-pic" src="https://cloud.githubusercontent.com/assets/15967809/17642775/7d354304-6171-11e6-8b56-66eee4681d88.png" alt="project">
-                </a>
+                <div class="container">
+                    <img class="project-pic" src="https://user-images.githubusercontent.com/7342669/49687801-61780100-fad6-11e8-992b-09e4cb900fdd.gif" alt="project">
+                    <div class="overlay">
+                        <div class="text">
+                            <a href="https://codepen.io/FreeCodeCamp/pen/KzXQgy" target="_blank">Link</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <a href="https://codepen.io/FreeCodeCamp/" class="show-all" target="_blank">Show all</a>
         </section>
 
-        <section id="contact" class="contact">
-            <div class="header">
-                <h1>Let's work together...</h1>
-                <p>How do you take your coffee?</p>
+        <section id="contact">
+            <div class="contact-header">
+                <h1>Want to learn more about me?</h1>
             </div>
-            <a href="https://facebook.com/freecodecamp" target="_blank" class="contact-details">Facebook</a>
-            <a id='profile-link' href="https://github.com/freecodecamp" target="_blank" class="contact-details">GitHub</a>
-            <a href="https://twitter.com/freecodecamp" target="_blank" class="contact-details">Twitter</a>
-            <a href="mailto:example@example.com" class="contact-details">Send a mail</a>
-            <a href="tel:555-555-5555" class="contact-details">Call me</a>
+            <a href="https://twitter.com/heatherakshar" target="_blank" class="contact-details">Twitter</a>
+            <a id="profile-link" href="https://github.com/heatheraislinn" target="_blank" class="contact-details">GitHub</a>
+            <a href="https://codepen.io/heatheraislinn/" target="_blank" class="contact-details">CodePen</a>
+            <a href="#" class="contact-details">LinkedIn</a>
+            <a href="mailto:heatheraislinn@gmail.com" class="contact-details">Email</a>
         </section>
 
-        <footer>**This is just a fake portfolio.. All the projects and contact details given are not real..
-            <p>&copy; Created for <a href="https://www.freecodecamp.com/" target="_blank">FreeCodeCamp</a></p>
+        <footer>
+            <p>&copy; Created by Heather Shar as part of the <a href="https://www.freecodecamp.com/" target="_blank">FreeCodeCamp</a> Responsive Web Design curriculum</p>
         </footer>
     </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Quicksand');
-
     html, body {
         font-family: 'Helvetica', Arial, sans-serif;
         text-align: center;
@@ -98,14 +141,13 @@
         background: #85d6bc;
         min-height: 500px;
         padding-top: 375px;
-        font-family: 'Quicksand', sans-serif;
         color: white;
-        font-weight: bold;
     }
 
     h1 {
         margin-top: 20px;
         font-size: 3em;
+        font-weight: normal;
     }
 
     .frontend {
@@ -180,59 +222,80 @@
     /* Projects Section */
 
     #projects {
-        padding-top: 40px;
-        padding-bottom: 40px;
+        padding-top: 60px;
+        padding-bottom: 60px;
         background: #ede4d7;
+    }
+
+    .projects-header {
+        font-weight: normal;
+        text-transform: uppercase;
+        margin-top: 10px;
     }
 
     .project-tile {
         display: grid;
         grid-template-columns: 50% 50%;
-        grid-template-rows: 450px 450px 450px 450px;
-        max-width: 800px;
+        grid-template-rows: 500px 450px 500px 550px;
+        max-width: 900px;
         justify-content: center;
         justify-items: center;
-        align-items: center;
         margin: 40px auto;
         grid-gap: 10px;
     }
 
+    .container {
+        position: relative;
+    }
+
     .project-pic {
-        width: 100%;
-        height: 100%;
-    }
-
-
-    .show-all {
-        text-decoration: none;
         display: block;
-        width: 200px;
-        height: 30px;
-        margin: 20px auto 90px;
-        background: #e0ebe8;
-        color: #008080;
-        font-weight: bold;
-        font-size: 1em;
-        border: 2px solid #90C695;
-        border-radius: 4px;
-        cursor: pointer;
-        padding-top:10px;
+        width: 100%;
+        height: auto;
     }
 
-    .show-all:hover {
-        background: #E4F1FE;
-        color: green;
+    .overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100%;
+        width: 100%;
+        opacity: 0;
+        transition: .5s ease;
+        background-color: #585858;
+    }
+
+    .container:hover .overlay {
+        opacity: 1;
+    }
+
+    .text {
+        color: white;
+        font-size: 20px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        text-align: center;
     }
 
     /* Contact Section */
 
-    .contact {
-        background: #C8F7C5;
+    #contact {
+        background: #85d6bc;
+        padding-top: 160px;
         padding-bottom: 160px;
+        color: white;
     }
 
-    .header {
-        padding: 160px 0 110px;
+    .contact-header {
+        padding-bottom: 60px;
+        text-transform: uppercase;
+        font-weight: normal;
     }
 
     .contact-details {
@@ -240,19 +303,19 @@
         font-weight: bold;
         margin: 0 35px 45px 35px;
         list-style-type: none;
-        border: 2px solid #90C695;
+        border: 2px solid white;
         border-radius: 100%;
         width: 100px;
         height: 100px;
         line-height: 100px;
-        background: #e0ebe8;
-        color: #008080;
+        background: #85d6bc;
+        color: white;
         text-decoration: none;
     }
 
     .contact-details:hover {
-        background: #E4F1FE;
-        color: green;
+        background: white;
+        color: #85d6bc;
     }
 
     /* Footer */
